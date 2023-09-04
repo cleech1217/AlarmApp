a8f010d1e5294de79a40a1f65701aae758636a56
Cliff <cacd1217@gmail.com>
2023-09-04 11:33:35 +0800

    New and improved alarm application

---
+++ AlarmV06.py
@@ -0,0 +1,71 @@
+import socket
+import threading
+from zeroconf import ServiceInfo, Zeroconf
+
+# Define the service name and port
+SERVICE_NAME = "MyService"
+PORT = 12345
+
+# Initialize Zeroconf
+zeroconf = Zeroconf()
+
+# Create and register the service with properties
+properties = {
+    "description": "My Service Description",
+    "path": "/my-service-path",
+}
+
+info = ServiceInfo("_myservice._tcp.local.",
+                   f"{SERVICE_NAME}._myservice._tcp.local.",
+                   addresses=[socket.inet_pton(socket.AF_INET, socket.gethostbyname(socket.gethostname()))],
+                   port=PORT,
+                   properties=properties)
+
+zeroconf.register_service(info)
+
+# Function to handle incoming connections and commands
+def handle_connection(client_socket):
+    while True:
+        try:
+            # Receive the command from the client
+            command = client_socket.recv(1024).decode('utf-8')
+            if not command:
+                break
+            
+            # Handle the command and send a response
+            if command == 'Alarm Status':
+                #set alarm status
+                response = 'Sending alarm status...'
+            elif command == 'Activate':
+                #Activate alarm
+                response = 'Alarm activate'
+            elif command == 'Deactivate':
+                #Deactivate alarm
+                response = 'Alarm inactivate'
+            else:
+                response = 'Unknown Command'
+            
+            # Send the response back to the client
+            client_socket.send(response.encode('utf-8'))
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+
+def main():
+    # Create a socket to listen for incoming connections
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', PORT))
+    server_socket.listen(5)
+
+    print(f"Listening for connections on port {PORT}...")
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f"Accepted connection from {addr[0]}:{addr[1]}")
+        
+        # Handle the connection in a new thread
+        client_thread = threading.Thread(target=handle_connection, args=(client_socket,))
+        client_thread.start()
+
+if __name__ == "__main__":
+    main()
---
+++ alarmTest.py
@@ -0,0 +1,78 @@
+import socket
+from zeroconf import Zeroconf, ServiceBrowser, ServiceStateChange
+
+# Define the service type to discover
+SERVICE_TYPE = "_myservice._tcp.local."
+
+# Initialize Zeroconf
+zeroconf = Zeroconf()
+
+# Function to handle service discovery
+class MyListener:
+    def __init__(self):
+        self.server_info = None
+
+    def remove_service(self, zeroconf, service_type, name):
+        pass
+
+    def add_service(self, zeroconf, service_type, name):
+        info = zeroconf.get_service_info(service_type, name)
+        if info:
+            self.server_info = info
+
+    def update_service(self, zeroconf, service_type, name):
+        pass
+
+# Discover services
+listener = MyListener()
+browser = ServiceBrowser(zeroconf, SERVICE_TYPE, listener)
+
+# Wait for the server info to be discovered
+while listener.server_info is None:
+    pass
+
+# Extract the server address and port from the discovered info
+server_addresses = [socket.inet_ntoa(addr) for addr in listener.server_info.addresses]
+server_port = listener.server_info.port
+
+# Use the first discovered address (you can modify this logic if there are multiple addresses)
+server_address = server_addresses[0] if server_addresses else None
+
+if server_address:
+    print(f"Discovered Server Address: {server_address}")
+    print(f"Discovered Server Port: {server_port}")
+else:
+    print("No server address found. Exiting.")
+    exit()
+
+# Function to send a command to the server
+def send_command(command):
+    try:
+        # Create a socket to connect to the server
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((server_address, server_port))
+
+        # Send the command to the server
+        client_socket.send(command.encode('utf-8'))
+
+        # Receive and print the server's response
+        response = client_socket.recv(1024).decode('utf-8')
+        print(f"Server Response: {response}")
+
+        # Close the client socket
+        client_socket.close()
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    while True:
+        # Input a command from the user
+        command = input("Enter a command (or 'exit' to quit): ")
+        if command.lower() == 'exit':
+            break
+
+        # Send the command to the discovered server
+        send_command(command)
+
+# Clean up Zeroconf
+zeroconf.close()
